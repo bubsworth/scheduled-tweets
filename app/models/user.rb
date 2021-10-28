1d9450928a1ddf06ae7faece4eb55b9c7b7e12f6
Beth Hubbard <bah1996@hotmail.co.uk>
2021-10-28 16:43:39 +0100

    Fix regex/email validation issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   # validations
-  validates :email, presence: :true
+  validates :email, presence: :true, format: { 
+    with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i , 
+    message: "Must be a valid email address." 
+  }
 end
