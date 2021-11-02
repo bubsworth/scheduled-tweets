82a0bdc2b54e89e86c0ceb9bebb01045b9d8daed
bubsworth <bah1996@hotmail.co.uk>
2021-11-02 16:37:33 +0000

    Create tweets wip

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,6 +10,21 @@ class TweetsController < ApplicationController
   end
   
   def create
+    @tweet = Current.user.tweets.new(tweet_params)
 
+    if @tweet.save
+      redirect_to tweets_path, notice: "Tweet was scheduled successfully"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
   end
 end
