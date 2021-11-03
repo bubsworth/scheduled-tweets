4ba74e4eb38b69a6531bb1d6de7821a471a0f423
bubsworth <bah1996@hotmail.co.uk>
2021-11-03 12:19:14 +0000

    Setup twitter API posting functionailty

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -1,8 +1,18 @@
 class TwitterAccount < ApplicationRecord
   # Associations
-  has_many :tweets
+  has_many :tweets, dependent: :destroy
   belongs_to :user
 
   # Validations
   validates :username, uniqueness: true
+
+  # Methods
+  def client
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.credentials.dig(:twitter, :api_key)
+      config.consumer_secret     = Rails.application.credentials.dig(:twitter, :api_key_secret)
+      config.access_token        = token
+      config.access_token_secret = secret
+    end
+  end
 end
